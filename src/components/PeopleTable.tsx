2e756592ee61b8849b78eecb119ad85608e15887
Jeny Berezhna <JenySmails24@gmail.com>
2026-01-21 11:24:33 +0000

    add task solution

---
+++ src/components/PeopleTable.tsx
@@ -0,0 +1,38 @@
+import { Person } from '../types/Person';
+
+type Props = {
+  people: Person[];
+};
+
+export const PeopleTable: React.FC<Props> = ({ people }) => {
+  return (
+    <table
+      data-cy="peopleTable"
+      className="table is-striped is-hoverable is-narrow is-fullwidth"
+    >
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Sex</th>
+          <th>Born</th>
+          <th>Died</th>
+          <th>Mother</th>
+          <th>Father</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        {people.map(person => (
+          <tr data-cy="person" key={person.slug}>
+            <td>{person.name}</td>
+            <td>{person.sex}</td>
+            <td>{person.born}</td>
+            <td>{person.died}</td>
+            <td>{person.motherName || '-'}</td>
+            <td>{person.fatherName || '-'}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};

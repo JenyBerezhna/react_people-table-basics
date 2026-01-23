351b53adf78ba849c253a96516eb24eae7877873
Jeny Berezhna <JenySmails24@gmail.com>
2026-01-23 19:12:11 +0000

    add task solution

--- src/types/Person.ts
+++ src/types/Person.ts
@@ -3,16 +3,9 @@ export interface Person {
   sex: string;
   born: number;
   died: number;
+  fatherName: string | null;
+  motherName: string | null;
   slug: string;
-
-  // API fields
-  mother: string | null; // slug
-  father: string | null; // slug
-
-  // Derived fields (filled in after processing)
-  motherName?: string | null;
-  fatherName?: string | null;
-
-  motherPerson?: Person | null;
-  fatherPerson?: Person | null;
+  mother?: Person;
+  father?: Person;
 }
